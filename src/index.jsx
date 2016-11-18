42b501c9cb1ea3ffa07e729a4f870a9a148f434b
heberlipe <heberlipe@hotmail.com>
2016-11-17 22:25:12 -0300

    Refresh only bootstrap

--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Principal} from './HelloWorld';
-
-ReactDOM.render(<Principal name="Diego" />, document.getElementById('root'));
