abdd899f5bdd114b7b31e748270390018e0ef4ce
heberlipe <heberlipe@hotmail.com>
2016-11-24 11:06:44 -0300

    update: resolver animaciones en react

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -4,38 +4,6 @@ export class Nav extends Component{
     render(){
       	return(
 
-            <div className="container">
-                <div className="navbar-header page-scroll">
-                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars"></i>
-                    </button>
-                    <a className="navbar-brand page-scroll" href="#page-top">Titulo</a>
-                </div>
-
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li className="hidden">
-                            <a href="#page-top"></a>
-                        </li>
-                        <li>
-                            <a className="page-scroll" href="#nosotros">Nosotros</a>
-                        </li>
-                        <li>
-                            <a className="page-scroll" href="#pizzas">Nuestras Pizzas</a>
-                        </li>
-                        <li>
-                            <a className="page-scroll" href="#empanadas">Nuestras Empanadas</a>
-                        </li>
-                        <li>
-                            <a className="page-scroll" href="#team">Equipo</a>
-                        </li>
-                        <li>
-                            <a className="page-scroll" href="#contact">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
       	);
     }
 }
@@ -43,13 +11,7 @@ export class Nav extends Component{
 export class Header extends Component{
   render(){
     return(
-      <div className="container">
-          <div className="intro-text">
-              <div className="intro-lead-in">Bienvenidos</div>
-              <div className="intro-heading">Alta pagina :D</div>
-              <a href="#nosotros" className="page-scroll btn btn-xl">Delivery</a>
-          </div>
-      </div>
+
     );
   }
 }
