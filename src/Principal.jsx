030aa3ab11277132176e3632f2dcacacff4ab60f
Heber Acosta <heber.acosta@globant.com>
2016-11-24 17:30:44 -0300

    Backup

---
+++ src/Principal.jsx
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+
+export class Nav extends Component{
+    render(){
+      	return(
+
+      	);
+    }
+}
+
+export class Header extends Component{
+  render(){
+    return(
+      <div class="container">
+          <div class="intro-text">
+              <div class="intro-lead-in">Bienvenidos</div>
+              <div class="intro-heading">Alta pagina :D</div>
+              <a href="#nosotros" class="page-scroll btn btn-xl">Delivery</a>
+          </div>
+      </div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Nav,Header} from './HelloWorld';
+import {Nav,Header} from './Principal';
 
 ReactDOM.render(<Nav/>, document.getElementById('mainNav'));
 ReactDOM.render(<Header/>, document.getElementById('header-section'));
