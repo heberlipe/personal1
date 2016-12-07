b210569234ac3fb7f705f38ea5fc4c314e9108e9
Heber Acosta <heber.acosta@globant.com>
2016-12-07 18:07:11 -0300

    upd

--- src/SubComponents.jsx
+++ src/SubComponents.jsx
@@ -5,13 +5,19 @@ export const Item = (props) =>{
   let ext = ".png"
   return(
     <div className="col-md-2 col-sm-2 portfolio-item">
-        <a href={props.modal} className="portfolio-link" data-toggle="modal">
+        <a href={props.modal}
+          className="portfolio-link"
+          data-toggle="modal">
             <div className="portfolio-hover">
                 <div className="portfolio-hover-content">
                     <i className="fa fa-plus fa-3x"></i>
                 </div>
             </div>
-            <img src={`${url}${props.title}${ext}`} className="img-responsive" alt=""/>
+            <img
+              src={`${url}${props.title}${ext}`}
+              className="img-responsive"
+              alt=""
+            />
         </a>
         <div className="portfolio-caption">
             <h4>{props.title}</h4>
